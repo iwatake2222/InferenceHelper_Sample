dd9b2002b439b2c11e39a553d4b3c0423a556176
iwatake2222 <take.iwiw2222@gmail.com>
2022-02-11 14:35:11 +0900

    [ci] fail on purpose

--- pj_cls_mobilenet_v2_wo_opencv/main.cpp
+++ pj_cls_mobilenet_v2_wo_opencv/main.cpp
@@ -25,7 +25,7 @@ limitations under the License.
 #include <algorithm>
 #include <chrono>
 #include <fstream>
-
+aaaaaaaa
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
